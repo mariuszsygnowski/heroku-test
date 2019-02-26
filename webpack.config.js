a79b670fe07ea77527c6454f0ad4a36661bc1c16
mariuszsygnowski <sygnowski.m@gmail.com>
2019-02-26 21:37:31 +0000

    test v3

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devtool: "source-map",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "build")
+    path: path.resolve(__dirname, "static")
   },
 
   module: {
